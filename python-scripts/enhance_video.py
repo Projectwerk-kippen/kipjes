e19963dd0e2ff76fc3e7cc56dafc307819e7c8e7
Andreas Deturck <andreas.deturck@gmail.com>
2020-03-18 13:31:18 +0100

    Changed improve_contrast to enhance_video

--- python-scripts/enhance_video.py
+++ python-scripts/enhance_video.py
@@ -10,30 +10,43 @@ Pieter Standaert, Dre Van Hoof, John Heymans, Brecht Grobben
 Supervisors: Vere Leybaert, Sara Verlinden, Tomas Norton, Erik Vranken, Meiqing Wang
 """
 
+# import os
 import cv2
-from tkinter.filedialog import askopenfile
+import tkinter as tk
+from tkinter import filedialog
 
 
 def choose_data():
-    data_video = askopenfile(mode='r')
+    data_video = filedialog.askopenfile(mode='r')
+    root = tk.Tk()
+    root.withdraw()
     return data_video.name
 
 
 def import_video():
     video_name = choose_data()
     cap = cv2.VideoCapture(video_name)
-    return cap
+    return cap, video_name
 
 
-def improve_contrast():
+def enhance_video():
+    cap, video_name = import_video()
 
-    cap = import_video()
+    # width = int(cap.get(3))
+    # height = int(cap.get(4))
+
+    # Define the codec and create VideoWriter object
+    # fourcc = cv2.VideoWriter_fourcc(*'DIVX')
+    # out = cv2.VideoWriter((os.path.split(video_name)[-1] + '_enhanced.avi'), fourcc, 20, (height, width), 0)
 
     while cap.isOpened():
         ret, frame = cap.read()
 
+        # Applying Gaussian low-pass filter to remove noise
+        blur = cv2.GaussianBlur(frame, (21, 21), 0)
+
         # Converting image to LAB Color model
-        lab = cv2.cvtColor(frame, cv2.COLOR_BGR2LAB)
+        lab = cv2.cvtColor(blur, cv2.COLOR_BGR2LAB)
 
         # Splitting the LAB image to different channels
         l, a, b = cv2.split(lab)
@@ -45,16 +58,19 @@ def improve_contrast():
         # Merge the CLAHE enhanced L-channel with the a and b channel
         limg = cv2.merge((cl, a, b))
 
-        # Converting image from LAB Color model to RGB model
-        final = cv2.cvtColor(limg, cv2.COLOR_LAB2BGR)
-        cv2.imshow('Improved contrast', final)
+        # Converting image from LAB Color model to grayscale model for thresholding
+        contrasted = cv2.cvtColor(limg, cv2.COLOR_LAB2BGR)
+        gray = cv2.cvtColor(contrasted, cv2.COLOR_BGR2GRAY)
 
-        if cv2.waitKey(25) & 0xFF == ord('q'):
+        cv2.imshow('Result', gray)
+
+        if cv2.waitKey(1) == 27:
             break
 
     cap.release()
+    out.release()
     cv2.destroyAllWindows()
 
 
 if __name__ == "__main__":
-    improve_contrast()
+    enhance_video()
